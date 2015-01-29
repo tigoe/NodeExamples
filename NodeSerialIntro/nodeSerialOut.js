b4bed4a4df79e3573a9afb054d56514a30163c11
tigoe <tom.igoe@gmail.com>
2015-01-28 20:11:50 -0500

    Moved NodeSerialIntro from NetworkExamples

---
+++ NodeSerialIntro/nodeSerialOut.js
@@ -0,0 +1,65 @@
+/*
+	nodeSerialOut.js
+	
+	Tests the functtionality of the serial port library.
+	To be used in conjunction with the Arduino sketch called AsciiSerialRead.ino
+	
+	This script sends a number out the serial port
+	every second.
+	
+	To call this from the command line:
+
+	node serialTest.js portname
+	 
+	where portname is the path to the serial port.
+	
+	created 17 June 2014
+	modified 28 Jan 2015
+	by Tom Igoe
+
+*/
+
+
+var serialport = require("serialport"),	// include the serialport library
+	SerialPort  = serialport.SerialPort,	// make a local instance of it
+	portName = process.argv[2];				// get the serial port name from the command line
+	
+	var brightness = 0;
+
+// open the serial port. The portname comes from the command line:
+var myPort = new SerialPort(portName, { 
+	baudRate: 9600,
+	// look for return and newline at the end of each data packet:
+	parser: serialport.parsers.readline('\r\n')
+});
+ 
+// called when the serial port opens:
+myPort.on('open', function() {
+	console.log('port open');
+	console.log('baud rate: ' + myPort.options.baudRate);
+	
+	function sendData() {
+	// convert the value to an ASCII string before sending it:
+		myPort.write(brightness.toString());
+		console.log("Sending " + brightness + " out the serial port");
+		// increment brightness by 10 points. Rollover if > 255:
+		if (brightness < 255) {
+			brightness+= 10;
+		} else {
+			brightness = 0;
+		}	
+	}
+	// set an interval to update the brightness 10 times per second:
+	setInterval(sendData, 100);
+});
+
+// called when the serial port closes:
+myPort.on('close', function() {
+	console.log('port closed');
+});
+
+// called when there's an error with the serial port:
+myPort.on('error', function(error) {
+	console.log('there was an error with the serial port: ' + error);
+	myPort.close();
+});
