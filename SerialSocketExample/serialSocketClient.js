0ab6478c8ec7122cf0a9f0001bbc06d641c4d051
tigoe <tom.igoe@gmail.com>
2015-02-03 22:10:32 -0500

    Updated SerialSocketClient

--- SerialSocketExample/serialSocketClient.js
+++ SerialSocketExample/serialSocketClient.js
@@ -2,19 +2,24 @@
 	SerialSocketClient.js	
 	
 	A simple serial-to-chat client in node.js. 
-	Connects to a TCP socket server on port 8001. Sends anything
-	that comes in the serial port to the server. If the server sends an 
-	'end' event, the program stops.
+	Connects to a TCP socket server on port 8080. Sends messages
+	that comes in the serial port to the server if the client
+	is connected.
+
 	
 	You can use this with the socketServer example found here:
 	https://github.com/tigoe/NodeExamples/blob/master/SocketExample/socketServer.js
+
+	You can also use the Arduino serial client found here:
+	https://github.com/tigoe/NodeExamples/blob/master/SerialSocketExample/SerialJoystickClient/SerialJoystickClient.ino
 	
 	created 25 Jan 2015
+	modified 3 Feb 2015
 	by Tom Igoe
 */
 
 var net = require('net'),				// make an instance of the net library
-	client = new net.Socket(),			// make a new instance of the socket class
+	client,									// make a new instance of the socket class
 	input = '';								// input string from the keyboard (STDIN)
 
 var serialport = require("serialport"),	// include the serialport library
@@ -22,6 +27,8 @@ var serialport = require("serialport"),	// include the serialport library
 	portName = process.argv[2];				// get the serial port name from the command line
 	serverAddress = process.argv[3], 		// get the server address from the command line
 	portNumber = process.argv[4];				// get the port number from the command line
+
+var connected = false;							// if client is connected
 
 // make sure the port name, the server address, and the port number are all filled in:
 if (!portName || !serverAddress || !portNumber) {
@@ -33,11 +40,11 @@ if (!portName || !serverAddress || !portNumber) {
 
 // open the serial port. The portname comes from the command line:
 var myPort = new SerialPort(portName, { 
-	baudRate: 9600,
+	baudRate: 9600 //,
 	// look for return and newline at the end of each data packet.
 	// if your serial device doesn't send carriage return and newline, 
 	// comment this line out (and the comma after 9600 above):
-	parser: serialport.parsers.readline("\r\n")
+	//parser: serialport.parsers.readline("\r\n")
 });
 
 	
@@ -45,53 +52,69 @@ var myPort = new SerialPort(portName, {
 myPort.on('open', function() {
 	console.log('port open');
 	console.log('baud rate: ' + myPort.options.baudRate);
-	// connect to the server on localhost:
-	client.connect(portNumber, serverAddress, login);	
 
+	// called when there's new incoming serial data:  
+	myPort.on('data', function(data) {		
+			if (connected) {
+				client.write(data);
+			}
+			
+			// run through all the bytes in the incoming data string:
+			for (var c=0; c<data.length; c++) {
+				// convert each byte to its unicode character:
+				var thisByte = String.fromCharCode(data[c]);
+				console.log(thisByte);
+				// if the byte is x and you're not connected, connect:
+				if (thisByte === 'x' && !connected) {
+					console.log("I am connecting");
+					// connect to the server:
+					client = new net.Socket(),
+					client.connect(portNumber, serverAddress, login);
+				} 
+			}
 	
-	// this function runs when the client successfully connects:
-	function login() {
-		client.setEncoding('utf8');	// encode everything sent by the client as a string
-		console.log('Connected');
-		client.write('Hello ' + client.localAddress);	// send a hello and your address
-	
-	  	// this function runs if the server sends data:
-		client.on('data', function(data) {
-			data = data.trim();							// trim any whitespace from the string
-			console.log('Server said: ' + data);	// print the message
-			myPort.write(data);
-		});
+		// this function runs when the client successfully connects:
+		function login() {
+			client.setEncoding('utf8');	// encode everything sent by the client as a string
+			console.log('Connected');
+			connected = true;
 		
-	  	// this function runs if the client ends, either locally
-	  	// or from the server:	
-		client.on('end', function() {
-			client.destroy();				// destroy the server connection 
-		});
-		
-		// called when there's new incoming serial data:  
-		myPort.on('data', function (data) {
-			// send the serial data to the server:
-			client.write(data);
-		});
-
-	}
+		  	// this function runs if the server sends data:
+			client.on('data', function(data) {
+				data = data.trim();							// trim any whitespace from the string
+				console.log('Server said ' + data);
+				// only send hi or bye on to the client, since Arduino code
+				// doesn't look for other messages:
+				if (data === 'hi' || data === 'bye') {
+					myPort.write(data);
+				}
+			});
+			
+		  	// this function runs if the client ends from the server:	
+			client.on('end', function() {
+				console.log('client ended');
+				connected = false;
+			});
+			
+			// this function runs after the client end event:
+			client.on('close', function() {
+				console.log('Connection closed');
+				connected = false;
+				// eliminate any reference to the client,
+				// so you can re-create it on next connect:
+				client = null;
+			});
+		}
+	});
 });
 
 // called when the serial port closes:
 myPort.on('close', function() {
-	console.log('port closed');
-	client.destroy();				// destroy the server connection			
+	console.log('port closed');		
 });
 
 // called when there's an error with the serial port:
 myPort.on('error', function(error) {
 	console.log('there was an error with the serial port: ' + error);
 	myPort.close();
-});
-
-
-// when the client closes, stop the program:
-client.on('close', function() {
-	console.log('Connection closed');
-	process.exit(0);
 });
