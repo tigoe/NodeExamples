9c8feb3d6de3ca3cfe1aba2dd67f517a6ac96fbb
tigoe <tom.igoe@gmail.com>
2015-02-10 08:08:39 -0500

    Added ExpressIntro examples

---
+++ ExpressIntro/getPost.js
@@ -0,0 +1,63 @@
+/*
+	Express.js GET/POST example
+	Shows how to get the parameters of a GET vs a POST request
+	in Express.js 4.0
+	
+	created 10 Feb 2015
+	by Tom Igoe
+*/
+
+var express = require('express');			// include express.js
+var app = express();								// a local instance of it
+var bodyParser = require('body-parser');	// include body-parser
+
+// you need a couple of parsers for the body of a POST request:
+app.use(bodyParser.json()); // for  application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for application/x-www-form-urlencoded
+
+// this runs after the server successfully starts:
+function serverStart() {
+  var port = server.address().port;
+  console.log('Server listening on port '+ port);
+}
+
+// this is called by both GET and POST handlers,
+// to format a response to the request:
+function formatResponse(thisContent) {
+	var result = 'You sent me:' + 
+  			'\n name: ' + thisContent.name +
+  			'\n age: ' + thisContent.age + '\n';
+  	return result;
+}
+
+// this is the GET handler:
+app.get('/', function (request, response) {
+	console.log('got a GET request');
+	// the parameters of a GET request are passed in
+	// request.query. Pass that to formatResponse()
+	// for formatting:
+	var content = formatResponse(request.query);
+	console.log(content);
+	
+	// send the response:
+	response.send(content);
+	response.end();
+});
+
+// accept POST request on the homepage
+app.post('/', function (request, response) {
+	console.log('Got a POST request');
+	// the parameters of a GET request are passed in
+	// request.body. Pass that to formatResponse()
+	// for formatting:
+	var content = formatResponse(request.body);
+	console.log(content);
+ 	
+	// send the response: 
+	response.send(content);
+	response.end();
+});
+
+// start the server:
+var server = app.listen(8080, serverStart);
+
---
+++ ExpressIntro/restParameters.js
@@ -0,0 +1,94 @@
+/*
+	Express.js REST parameters example
+	Shows how to take values in a RESTful way
+	in Express.js 4.0
+	
+	This does no checking on the data sent, it just takes
+	the values in and adds them to arrays.
+	
+	created 10 Feb 2015
+	by Tom Igoe
+*/
+
+var express = require('express');	// include express.js
+var app = express();						// a local instance of it
+var names = [];							// an array for name values from the client	
+var ages = [];								// an array of age values from the client
+	
+// this runs after the server successfully starts:
+function serverStart() {
+  var port = server.address().port;
+  console.log('Server listening on port '+ port);
+}
+
+// this is the handler for the root of the site:
+app.get('/', function (request, response) {	
+	var content = 'Hello. Would you like to know the name or the age?';
+	content += '\n';			// add a newline at the end of the content
+	response.send(content);	// send it back to the client
+	response.end();			// close the connection
+});
+
+// this is the handler for /name with a parameter:
+app.get('/name/:name', function (request, response) {	
+	var content = 'The name you gave me is: ';
+	var name = request.params.name;
+	names.push(name);
+	content += name;
+	content += '\n';			// add a newline at the end of the content
+	response.send(content);	// send it back to the client
+	response.end();			// close the connection
+});
+
+// this is the handler for /name:
+app.get('/name/', function (request, response) {	
+	var content = 'The last name you gave me is: ';
+	var name = names[names.length -1];
+	content += name;
+	content += '\n';			// add a newline at the end of the content
+	response.send(content);	// send it back to the client
+	response.end();			// close the connection
+});
+
+// this is the handler for /names:
+app.get('/names/', function (request, response) {	
+	var content = 'The names are: ';
+	content += names;
+	content += '\n';			// add a newline at the end of the content
+	response.send(content);	// send it back to the client
+	response.end();			// close the connection
+});
+
+// this is the handler for the /age with a parameter:
+app.get('/age/:age', function (request, response) {
+	var content = 'The age you gave me is: ' ;
+	var age = request.params.age;
+	ages.push(age);
+	content += age;
+	content += '\n';			// add a newline at the end of the content
+	response.send(content);	// send it back to the client
+	response.end();			// close the connection
+});
+
+// this is the handler for /age:
+app.get('/age/', function (request, response) {	
+	var content = 'The last age you gave me is: ';
+	var age = ages[ages.length -1];
+	content += age;
+	content += '\n';			// add a newline at the end of the content
+	response.send(content);	// send it back to the client
+	response.end();			// close the connection
+});
+
+// this is the handler for the /ages:
+app.get('/ages/', function (request, response) {
+	var content = 'The ages are: ' ;
+	content += ages;
+	content += '\n';			// add a newline at the end of the content
+	response.send(content);	// send it back to the client
+	response.end();			// close the connection
+});
+
+// start the server:
+var server = app.listen(8080, serverStart);
+
