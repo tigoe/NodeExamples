e96d9dcdd1d50e460dd51d748e4c6c1838cf8a9c
tigoe <tom.igoe@gmail.com>
2015-02-24 19:30:49 -0500

    Updated socketIO server example

--- SocketIO/server.js
+++ SocketIO/server.js
@@ -6,53 +6,40 @@
 	The client uses p5.js (http://p5js.org/) to generate its UI and handle user events
 	
 	created 13 Jan 2015
+	modified 24 Feb 2015
 	by Tom Igoe
-	
 */
 
-
 var express = require('express');			// include express.js
-var app = express();								// make an instance of express.js
-var http = require('http').Server(app);	// include http and make a server instance with the express instance
-//var io = require('socket.io')(http);		// include socket.io and make a socket server instance with the http instance
+var app = express();						// make an instance of express.js
+var http = require('http').Server(app);		// include http and make a server instance with the express instance
+var io = require('socket.io')(http);		// include socket.io and make a socket server instance with the http instance
 
 // send the index page if you get a request for / :
 app.get('/', sendIndex);
-
-app.get('/*', respondToClient);
 
 // callback function for 'get /' requests:
 function sendIndex(request, response){
 	response.sendFile(__dirname + '/index.html');
 }
 
-function respondToClient(request, response) {  
+io.on('connection', function(socket){
+	console.log('a user connected');
+	console.log(socket.handshake.address);
+	// send something to the web client with the data:
+	socket.emit('message', "Hello, " + socket.handshake.address);
+	socket.on('message', function(data) {
+		console.log('received from client: ' + data);
 
-	console.log("request from: ");
-	console.log(request.connection.remoteAddress);
-	
-	console.log("headers:");
-	console.log(request.headers);
-	
-	console.log("URL:");
-	console.log(request.url);
-
-	
-	// if you got a POST request, here's the body
-	request.on('data', function(body) {
-		console.log("Body of request:");
-		console.log(body.toString());
-	});
-	
-	// write back to the client:
-	response.writeHead(200, {"Content-Type": "text/html"});  
-	response.write("Hello, " + request.connection.remoteAddress);
-	response.write("hits: " + hitCount);
-	response.end();
-	// increment the hit counter:
-	hitCount++;
-};
-
+		// This function sends the time out an open socket:
+		function sendTime() {
+			var message = "Time and date from the server: " + new Date();
+			socket.emit('message',message);
+		}
+		// when a socket's open, send the time once a second:
+		setInterval(sendTime, 1000);
+	}); 
+});
 
 // listen for incoming server messages:
 http.listen(8080, function(){
