b3cef5171f707d4893773dbab46fa04b57fbb671
tigoe <tom.igoe@gmail.com>
2015-03-17 17:59:17 -0400

    formatting cleanup restParameters

--- ExpressIntro/restParameters.js
+++ ExpressIntro/restParameters.js
@@ -2,19 +2,19 @@
 	Express.js REST parameters example
 	Shows how to take values in a RESTful way
 	in Express.js 4.0
-	
+
 	This does no checking on the data sent, it just takes
 	the values in and adds them to arrays.
-	
+
 	created 10 Feb 2015
 	by Tom Igoe
 */
 
 var express = require('express');	// include express.js
-var app = express();						// a local instance of it
-var names = [];							// an array for name values from the client	
-var ages = [];								// an array of age values from the client
-	
+var app = express();							// a local instance of it
+var names = [];										// an array for name values from the client
+var ages = [];										// an array of age values from the client
+
 // this runs after the server successfully starts:
 function serverStart() {
   var port = server.address().port;
@@ -22,7 +22,7 @@ function serverStart() {
 }
 
 // this is the handler for the root of the site:
-app.get('/', function (request, response) {	
+app.get('/', function (request, response) {
 	var content = 'Hello. Would you like to know the name or the age?';
 	content += '\n';			// add a newline at the end of the content
 	response.send(content);	// send it back to the client
@@ -30,7 +30,7 @@ app.get('/', function (request, response) {
 });
 
 // this is the handler for /name with a parameter:
-app.get('/name/:name', function (request, response) {	
+app.get('/name/:name', function (request, response) {
 	var content = 'The name you gave me is: ';
 	var name = request.params.name;
 	names.push(name);
@@ -41,7 +41,7 @@ app.get('/name/:name', function (request, response) {
 });
 
 // this is the handler for /name:
-app.get('/name/', function (request, response) {	
+app.get('/name/', function (request, response) {
 	var content = 'The last name you gave me is: ';
 	var name = names[names.length -1];
 	content += name;
@@ -51,7 +51,7 @@ app.get('/name/', function (request, response) {
 });
 
 // this is the handler for /names:
-app.get('/names/', function (request, response) {	
+app.get('/names/', function (request, response) {
 	var content = 'The names are: ';
 	content += names;
 	content += '\n';			// add a newline at the end of the content
@@ -71,7 +71,7 @@ app.get('/age/:age', function (request, response) {
 });
 
 // this is the handler for /age:
-app.get('/age/', function (request, response) {	
+app.get('/age/', function (request, response) {
 	var content = 'The last age you gave me is: ';
 	var age = ages[ages.length -1];
 	content += age;
@@ -91,4 +91,3 @@ app.get('/ages/', function (request, response) {
 
 // start the server:
 var server = app.listen(8080, serverStart);
-
