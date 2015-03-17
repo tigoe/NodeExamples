c5cab694e04d33ca5c3ab31ed89e90ce3823c97c
tigoe <tom.igoe@gmail.com>
2015-03-17 17:59:41 -0400

    formatting cleanup routes.js

--- ExpressIntro/routes.js
+++ ExpressIntro/routes.js
@@ -8,7 +8,7 @@
 */
 
 var express = require('express');	// include express.js
-var app = express();						// a local instance of it
+var app = express();				// a local instance of it
 var names = ['Andres', 'Bob', 'Carol', 'Daniela', 'Ed', 'Francesca'];							
 	
 	
@@ -21,7 +21,7 @@ function serverStart() {
 // this is the handler for the root of the site:
 app.get('/', function (request, response) {	
 	var content = 'Hello. Would you like to know the name or the age?';
-	content += '\n';			// add a newline at the end of the content
+	content += '\n';		// add a newline at the end of the content
 	response.send(content);	// send it back to the client
 	response.end();			// close the connection
 });
@@ -32,7 +32,7 @@ app.get('/name/', function (request, response) {
 	var whichName = Math.floor(Math.random() * names.length);
 	var name = names[whichName];
 	content += name;
-	content += '\n';			// add a newline at the end of the content
+	content += '\n';		// add a newline at the end of the content
 	response.send(content);	// send it back to the client
 	response.end();			// close the connection
 });
@@ -42,7 +42,7 @@ app.get('/age/', function (request, response) {
 	var age = parseInt(Math.random() * 75);
 	var content = 'The age is: ' ;
 	content += age;
-	content += '\n';			// add a newline at the end of the content
+	content += '\n';		// add a newline at the end of the content
 	response.send(content);	// send it back to the client
 	response.end();			// close the connection
 });
