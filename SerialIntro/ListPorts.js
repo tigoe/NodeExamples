e2a40274db6fc64aa9baeb29c250b1f830d8a454
tigoe <tom.igoe@gmail.com>
2015-03-17 17:55:55 -0400

    updates to SerialIntro listPorts

--- SerialIntro/ListPorts.js
+++ SerialIntro/ListPorts.js
@@ -1,20 +1,21 @@
 /*
-	Lists serial ports. Copied from https://github.com/voodootikigod/node-serialport readme
-	
-	refactored to get rid of anonymous functions, to make it clearer for
-	those new to JavaScript
+Lists serial ports.
+Copied from https://github.com/voodootikigod/node-serialport readme
+
+refactored to get rid of anonymous functions, to make it clearer for
+those new to JavaScript
 */
 
-  var serialport = require("serialport");
-  var SerialPort = serialport.SerialPort; // localize object constructor
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort; // localize object constructor
 
 // list serial ports:
 serialport.list(listPorts);
 
 function listPorts(err, ports) {
-    ports.forEach(printPort);
-    
-    function printPort(port) {
-      console.log(port.comName + "\t" + port.manufacturer);
-    }
-  }
+	ports.forEach(printPort);
+
+	function printPort(port) {
+		console.log(port.comName + "\t" + port.manufacturer);
+	}
+}
