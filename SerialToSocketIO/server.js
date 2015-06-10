26badf84559947a1696ec437fe7753716698966e
tigoe <tom.igoe@gmail.com>
2015-06-10 18:29:59 -0400

    updated SerialToSocketIO example

--- SerialToSocketIO/server.js
+++ SerialToSocketIO/server.js
@@ -12,19 +12,16 @@ by Tom Igoe
 */
 
 var express = require('express');		// include express.js
-var app = express();					// make an instance of express.js
-var http = require('http').Server(app);	// include http and make a server instance with the express instance
-var io = require('socket.io')(http);	// include socket.io and make a socket server instance with the http instance
+var io = require('socket.io')				// include socket.io
+var app = express();								// make an instance of express.js
+var server = app.listen(8080);			// start a server with the express instance
+var socketServer = io(server);	 		// make a socket server using the express server
 
-app.use(express.static('public'));
+app.use(express.static('public'));	// serve files from the public folder
 
 var serialport = require("serialport"),	// include the serialport library
 SerialPort  = serialport.SerialPort,	// make a local instance of serial
 portName = process.argv[2];				// get the port name from the command line
-
-
-// send the index page if you get a request for / :
-app.get('/files/:name', serveFiles);	// GET handler for all static files
 
 
 // open the serial port. Uses the command line parameter:
@@ -34,6 +31,10 @@ var myPort = new SerialPort(portName, {
 	parser: serialport.parsers.readline('\n')
 });
 
+app.get('/:name', serveFiles);								// listener for all static file requests
+socketServer.on('connection', newConnection);	// listener for websocket data
+
+
 function serveFiles(request, response) {
 	// get the file name from the request parameter:
 	var fileName = request.params.name;
@@ -41,13 +42,12 @@ function serveFiles(request, response) {
 	response.sendFile(fileName);
 }
 
-// this is called when there's a websocket connection:
-io.on('connection', function(socket){
-	console.log('a user connected');
-	console.log(socket.handshake.address);
+function newConnection(socket){
+	console.log('new user address: ' + socket.handshake.address);
 	// send something to the web client with the data:
 	socket.emit('message', "Hello, " + socket.handshake.address);
 
+	// this function runs if there's input from the client:
 	socket.on('message', function(data) {
 		console.log('received from client: ' + data);
 	});
@@ -57,10 +57,4 @@ io.on('connection', function(socket){
 		// send the data and close the connection:
 		socket.emit('message', data);
 	});
-});
-
-
-// listen for incoming server messages:
-http.listen(8080, function(){
-	console.log('listening on port 8080');
-});
+}
