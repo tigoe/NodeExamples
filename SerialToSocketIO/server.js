bf30995686a26688acf3ab2027509a3eebf71548
tigoe <tom.igoe@gmail.com>
2015-06-10 20:09:08 -0400

    Formatting updates

--- SerialToSocketIO/server.js
+++ SerialToSocketIO/server.js
@@ -7,54 +7,57 @@ using Socket.io version 1.0 or later (http://socket.io/)
 Takes data from the command line to drive a graphic
 generated in  p5.js (http://p5js.org/)
 
+To run it from the command line:
+node server.js serialport
+
+where serialport is the name of your serial port.
+
 created 10 Jun 2015
 by Tom Igoe
 */
 
+// server initialization:
 var express = require('express');		// include express.js
-var io = require('socket.io')				// include socket.io
-var app = express();								// make an instance of express.js
-var server = app.listen(8080);			// start a server with the express instance
-var socketServer = io(server);	 		// make a socket server using the express server
+	io = require('socket.io'),				// include socket.io
+	app = express(),									// make an instance of express.js
+ 	server = app.listen(8080),				// start a server with the express instance
+	socketServer = io(server);	 			// make a socket server using the express server
 
-app.use(express.static('public'));	// serve files from the public folder
+// serial port initialization:
+var serialport = require('serialport'),			// include the serialport library
+	SerialPort  = serialport.SerialPort,			// make a local instance of serial
+	portName = process.argv[2],								// get the port name from the command line
+	portConfig = {
+		baudRate: 9600,
+		// call myPort.on('data') when a newline is received:
+		parser: serialport.parsers.readline('\n')
+	};
 
-var serialport = require("serialport"),	// include the serialport library
-SerialPort  = serialport.SerialPort,	// make a local instance of serial
-portName = process.argv[2];				// get the port name from the command line
+// open the serial port:
+var myPort = new SerialPort(portName, portConfig);
 
-
-// open the serial port. Uses the command line parameter:
-var myPort = new SerialPort(portName, {
-	baudRate: 9600,
-	// look for return and newline at the end of each data packet:
-	parser: serialport.parsers.readline('\n')
-});
-
-app.get('/:name', serveFiles);								// listener for all static file requests
-socketServer.on('connection', newConnection);	// listener for websocket data
-
+//  set up server and socketServer listener functions:
+app.use(express.static('public'));					// serve files from the public folder
+app.get('/:name', serveFiles);							// listener for all static file requests
+socketServer.on('connection', openSocket);	// listener for websocket data
 
 function serveFiles(request, response) {
-	// get the file name from the request parameter:
-	var fileName = request.params.name;
-	// send the file:
-	response.sendFile(fileName);
+	var fileName = request.params.name;				// get the file name from the request
+	response.sendFile(fileName);							// send the file
 }
 
-function newConnection(socket){
+function openSocket(socket){
 	console.log('new user address: ' + socket.handshake.address);
 	// send something to the web client with the data:
-	socket.emit('message', "Hello, " + socket.handshake.address);
+	socket.emit('message', 'Hello, ' + socket.handshake.address);
 
 	// this function runs if there's input from the client:
 	socket.on('message', function(data) {
-		console.log('received from client: ' + data);
+		myPort.write(data);							// send the data to the serial device
 	});
 
 	// this function runs if there's input from the serialport:
 	myPort.on('data', function(data) {
-		// send the data and close the connection:
-		socket.emit('message', data);
+		socket.emit('message', data);		// send the data to the client
 	});
 }
