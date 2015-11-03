e50fee4a5bb11b0bb48efbacf827c6daaaa14197
tigoe <tom.igoe@gmail.com>
2015-11-02 21:26:24 -0500

    updated ExpressIntro examples

--- ExpressIntro/routes.js
+++ ExpressIntro/routes.js
@@ -2,16 +2,17 @@
 	Express.js routes example
 	Shows how to respond to different routes
 	in Express.js 4.0
-	
+
 	created 10 Feb 2015
+	modified 2 Nov 2015
 	by Tom Igoe
 */
 
 var express = require('express');	// include express.js
 var app = express();				// a local instance of it
-var names = ['Andres', 'Bob', 'Carol', 'Daniela', 'Ed', 'Francesca'];							
-	
-	
+var names = ['Andres', 'Bob', 'Carol', 'David', 'Ed', 'Francesca'];
+
+
 // this runs after the server successfully starts:
 function serverStart() {
   var port = server.address().port;
@@ -19,7 +20,7 @@ function serverStart() {
 }
 
 // this is the handler for the root of the site:
-app.get('/', function (request, response) {	
+app.get('/', function (request, response) {
 	var content = 'Hello. Would you like to know the name or the age?';
 	content += '\n';		// add a newline at the end of the content
 	response.send(content);	// send it back to the client
@@ -27,7 +28,7 @@ app.get('/', function (request, response) {
 });
 
 // this is the handler for /name:
-app.get('/name/', function (request, response) {	
+app.get('/name/', function (request, response) {
 	var content = 'The name is: ';
 	var whichName = Math.floor(Math.random() * names.length);
 	var name = names[whichName];
@@ -38,7 +39,7 @@ app.get('/name/', function (request, response) {
 });
 
 // this is the handler for the /age:
-app.get('/age/', function (request, response) {	
+app.get('/age/', function (request, response) {
 	var age = parseInt(Math.random() * 75);
 	var content = 'The age is: ' ;
 	content += age;
@@ -49,4 +50,3 @@ app.get('/age/', function (request, response) {
 
 // start the server:
 var server = app.listen(8080, serverStart);
-
