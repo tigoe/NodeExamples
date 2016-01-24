26925e4a7fefd8b76f555f9368a3ef7a960a700a
jefarrell <johnefarrell18@gmail.com>
2016-01-23 20:34:59 -0500

    adding version using Request

---
+++ EnertivClient/requestVersion/app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var client = require('./client.js');
+var app = express();
+var c = new client;
+
+
+// Examples using Enertiv node module with Express
+// See https://api.enertiv.com/docs/#!/api/ for available endpoints
+
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('app listening at http://%s:%s', host, port);
+});
+
+
+/*
+	*
+	*		Important Info
+	*
+	*		Must hit '/login' first to authenticate	
+	*			- Follow setup in 'client.js'
+	*		Most API endpoints use client info (client/location uuid)
+	*		So, use '/client' to save that info for later use
+	*
+*/
+
+var clientData = {};
+
+// Hit this first to authenticate
+app.get('/login', function (req,res,next){
+	var data = c.login(function (data){
+		console.log("AUTHENTICATED");
+		res.send("YOU ARE AUTHENTICATED");
+		res.end();
+	});
+});
+
+
+// Returns client information, saves the important bits for later
+app.get('/client', function (req,res){
+	var apiClient = c.apiCall('/api/client/', function (apiClient){
+		var clientInfo = JSON.parse(apiClient);
+		console.log(clientInfo[0]);
+		clientData.uuid = clientInfo[0].uuid;
+		clientData.locationID = clientInfo[0].locations[0];
+		res.send(clientInfo[0]);
+		res.end();
+	});
+});
+
+
+
+// Example showing top 10 energy consumers
+// Uses the client uuid we saved from '/client'
+app.get('/top10', function (req,res,next){
+	// Set the date range you want to examine
+	var start = encodeURIComponent('2015-01-01 00:00:00Z');
+	var end = encodeURIComponent('2015-09-01 00:00:00Z');
+	var client = clientData.uuid;
+
+	var top10 = c.apiCall('/api/client/'+client+'/top_consumers/?count=10&fromTime='+start+'&toTime='+end, function (top10){
+		console.log(JSON.parse(top10));
+		res.send(JSON.parse(top10));
+		res.end();
+
+	})
+});
+
+
+// Get energy and cost data by location
+// Uses the location uuid we saved from '/client'
+app.get('/energy', function (req,res,next){
+	var location = clientData.locationID;
+	var startdate = encodeURIComponent('2015-01-01 00:00:00Z');
+	var enddate = encodeURIComponent('2015-09-01 00:00:00Z')
+	var interval = 'month';
+	var energyData = c.apiCall('/api/location/'+location+'/data/?fromTime='+startdate+'&toTime='+enddate+'&interval='+interval+'&cost=true', function (energyData){
+	    console.log(JSON.parse(energyData));
+	    res.send(JSON.parse(energyData));
+		res.end();
+	});
+});
+
+
+
---
+++ EnertivClient/requestVersion/client.js
@@ -0,0 +1,89 @@
+/*
+  HTTPS request example to Enertiv
+  Uses the Request module
+  You'll need to add a file, cred.js, to the same directory as this file,
+  with the following in it:
+
+  var creds = {
+      username : your_username,
+      password : your_password,
+      clientID : your_client_id,
+      clientSecret : your_client_secret
+  }
+  module.exports = creds;
+
+  created 23 Jan 2016 by John Farrell
+*/
+
+var querystring = require('querystring');
+var request = require('request');
+var cred = require('./cred.js');
+
+// Read login data from cred.js file (see above)
+var loginData = {
+	'host': 'https://api.enertiv.com',
+	'client_id': cred.clientID,
+	'client_secret': cred.clientSecret,
+	'grant_type': 'password',
+	'username': cred.username,
+	'password': cred.password
+};
+
+
+// module to export
+var enertiv = function() {
+	var self = this;
+    var callback;
+    var accessToken;
+
+    // Function to login to API
+    // Uses loginData object 
+    this.login = function(cb){
+    	callback = cb;
+    	request.post({
+			headers: {
+		    	'Content-Type': 'application/x-www-form-urlencoded'
+		    	},
+		    uri: loginData.host + '/oauth2/access_token/',
+		    body: querystring.stringify(loginData)
+			},
+			self.saveToken
+			);
+	};
+
+	// Saves token from Login response
+	// Called at the end of this.login()
+    this.saveToken = function (error, response, body) {
+		if (error) {console.log(error)}
+	  	else if (!error && response.statusCode == 200) {
+	   		var info = JSON.parse(body);
+	    	accessToken = info.access_token;
+	    	callback();
+	  	}
+	 };
+
+	// Generic API call function
+	// Makes use of previously saved token
+    this.apiCall = function(path, cb){
+    	callback = cb;
+		request({
+			headers:{
+				'Authorization': 'Bearer ' + accessToken
+				},
+			uri: loginData.host + path
+			},
+		function (error, response, body){
+			console.log(error);
+			console.log(body);
+			callback(body);
+			}
+		);
+	};
+};
+
+
+
+module.exports = enertiv;
+
+
+
