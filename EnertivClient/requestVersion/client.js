17b827e8557bd4ace76e2f2f10fe73badd75c4ec
jefarrell <johnefarrell18@gmail.com>
2016-02-17 15:50:00 -0500

    fixing req version

--- EnertivClient/requestVersion/client.js
+++ EnertivClient/requestVersion/client.js
@@ -1,11 +1,8 @@
 /*
-  HTTP/HTTPS request example to Enertiv
-  Does basic Oauth2 exchange to get access token
-  then makes first API request with the token.
-  This is a quick-and-dirty solution, and should be improved upon.
+  HTTPS request example to Enertiv
+  Uses the Request module
   You'll need to add a file, cred.js, to the same directory as this file,
   with the following in it:
-
   var creds = {
       username : your_username,
       password : your_password,
@@ -13,103 +10,75 @@
       clientSecret : your_client_secret
   }
   module.exports = creds;
-
-  created 25 Feb 2015 by Tom Igoe
-  updated 06 Jan 2016 by John Farrell
+  created 23 Jan 2016 by John Farrell
 */
 
-
-var https = require('https');
 var querystring = require('querystring');
+var request = require('request');
 var cred = require('./cred.js');
-var clientData;
 
-// Bring in login information from our cred file
-var loginData = querystring.stringify({
-    'client_id': cred.clientID,
-    'client_secret': cred.clientSecret,
-    'grant_type': 'password',
-    'username': cred.username,
-    'password': cred.password
-  });
-
-// set up the HTTPS request options. You'll modify and
-// reuse this for subsequent calls:
-var options = {
-  rejectUnauthorized: false,
-  method: 'POST',
-  host: 'api.enertiv.com',
-  port: 443,
-  path: '/oauth2/access_token/',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': loginData.length
-  }
+// Read login data from cred.js file (see above)
+var loginData = {
+  'host': 'https://api.enertiv.com',
+  'client_id': cred.clientID,
+  'client_secret': cred.clientSecret,
+  'grant_type': 'password',
+  'username': cred.username,
+  'password': cred.password
 };
 
-// Module
-var enertiv = function(){
+
+// module to export
+var enertiv = function() {
   var self = this;
-  var callback;
-  var accessToken;
+    var callback;
+    var accessToken;
 
-  // Authenticate with Enertiv API
-  // Login runs callback to saveToken
-  this.login = function(cb){
-    callback = cb;
-    var request = https.request(options, self.saveToken);  // start it
-      request.write(loginData);                           // add  body of  POST request
-      request.end();   
+    // Function to login to API
+    // Uses loginData object 
+    this.login = function(cb){
+      callback = cb;
+      request.post({
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+          },
+        uri: loginData.host + '/oauth2/access_token/',
+        body: querystring.stringify(loginData)
+      },
+      self.saveToken
+      );
   };
 
-  // Parse response and save auth token
-  // Pass that token to further API calls
-  this.saveToken = function(response){
-    var result = '';    // String to hold the response
-    // As each chunk comes in, add it to the result string:
-    response.on('data', function (data) {
-      result += data;
-    });
+  // Saves token from Login response
+  // Called at the end of this.login()
+    this.saveToken = function (error, response, body) {
+    if (error) {console.log(error)}
+      else if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body);
+        accessToken = info.access_token;
+        callback();
+      }
+   };
 
-    // When the final chunk comes in, grab the access token
-    // Then run our callback function
-    response.on('end', function () {
-      result = JSON.parse(result);
-      accessToken = result.access_token;
-      callback();  
-    });
+  // Generic API call function
+  // Makes use of previously saved token
+    this.apiCall = function(path, cb){
+      callback = cb;
+    request({
+      headers:{
+        'Authorization': 'Bearer ' + accessToken
+        },
+      uri: loginData.host + path
+      },
+    function (error, response, body){
+      console.log(error);
+      console.log(body);
+      callback(body);
+      }
+    );
   };
+};
 
-  // Generic function for API calls using access token
-  this.apiCall = function (path, cb){
 
-    callback = cb;    
-    options.method = 'GET';  // Change to a GET request
-    options.path = path;     // Set our path to the argument
-    options.headers = {      // Change authorization header to include our token
-      'Authorization': 'Bearer ' + accessToken
-    }
-
-    // Make the API call
-    request = https.get(options, function (response) { 
-      var result = '';
-      // As each chunk comes in, add it to the result string:
-      response.on('data', function (data) {
-        result += data;
-      });
-
-      // When the final chunk comes in, print it out
-      // Then run our callback function
-      response.on('end', function () {
-        callback(result);
-      });
-    });
-  };
-}
 
 module.exports = enertiv;
-
-
-
-
-
