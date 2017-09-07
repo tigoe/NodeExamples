0492dc16a62a71d581b937da36e530ad2d0fde5e
tigoe <tom.igoe@gmail.com>
2017-09-07 16:50:34 -0400

    updated socketExample files

--- SocketExample/socketClient.js
+++ SocketExample/socketClient.js
@@ -1,21 +1,21 @@
 /*
-	socketClient.js	
-	
-	A simple chat client in node.js. 
-	Connects to a TCP socket server on port 8001. Sends anything
-	typed on the keyboard to the server. If the server sends an 
-	'end' event, the program stops.
-	
-	created 25 Jan 2015
-	modified 3 Feb 2015
-	by Tom Igoe
+socketClient.js
+
+A simple chat client in node.js.
+Connects to a TCP socket server on port 8001. Sends anything
+typed on the keyboard to the server. If the server sends an
+'end' event, the program stops.
+
+created 25 Jan 2015
+modified 7 Sept 2017
+by Tom Igoe
 */
 
 var net = require('net'),				// make an instance of the net library
-	client = new net.Socket(),			// make a new instance of the socket class
-	input = '',								// input string from the keyboard (STDIN)
-	serverAddress = process.argv[2], // get the server address from the command line
-	portNumber = process.argv[3];		// get the port number from the command line
+client = new net.Socket(),			// make a new instance of the socket class
+input = '',											// input string from the keyboard (STDIN)
+serverAddress = process.argv[2],// get the server address from the command line
+portNumber = process.argv[3];		// get the port number from the command line
 
 // make sure the server address, and the port number are filled in:
 if (!serverAddress || !portNumber) {
@@ -24,39 +24,37 @@ if (!serverAddress || !portNumber) {
 	process.exit(0);
 }
 
-var stdin = process.openStdin();		// enable input from the keyboard
+var stdin = process.openStdin();	 // enable input from the keyboard
 stdin.setEncoding('utf8');			   // encode everything typed as a string
+client.setEncoding('utf8');				 // encode everything sent by the client as a string
 
-	
 client.connect(portNumber, serverAddress, login);	// connect to the server
+
+// client event handlers:
+client.on('data', printIncoming);	// if there's incoming data in the socket
+client.on('end', endClient);			// if the socket ends from the remote side
+client.on('close', endClient);		// if the socket ends from the local side
 
 // this function runs when the client successfully connects:
 function login() {
-	client.setEncoding('utf8');	// encode everything sent by the client as a string
 	console.log('Connected');
 	client.write('Hello ' + client.localAddress);	// send a hello and your address
-
-  	// this function runs if the server sends data:
-	client.on('data', function(data) {
-		data = data.trim();							// trim any whitespace from the string
-		console.log('Server said: ' + data);	// print the message
-	});
-	
-  	// this function runs if the client ends, either locally
-  	// or from the server:	
-	client.on('end', function() {
-		console.log("socket closed"); 
-	});
-	
-	// this function runs if there's input from the keyboard.
-	// you need to hit enter to generate this event.
-	stdin.on('data', function(data) {
-		client.write(data);							// send it to the server
-	});
 }
- 
-// when the client closes, stop the program:
-client.on('close', function() {
+
+// this function runs if there's input from the keyboard.
+// you need to hit enter to generate this event.
+stdin.on('data', function(data) {
+	client.write(data);							// send it to the server
+});
+
+// this function runs if there's incoming data from the socket:
+function printIncoming(data) {
+	data = data.trim();										// trim any whitespace from the string
+	console.log('Server said: ' + data);	// print the message
+}
+
+// this function runs if the socket is closed:
+function endClient() {
 	console.log('Connection closed');
 	process.exit(0);
-});
+}
--- SocketExample/socketServer.js
+++ SocketExample/socketServer.js
@@ -6,77 +6,86 @@ Prints all client text to the console. Keeps track of clients
 in an array. Typing 'c' will print the array of clients
 
 created 25 Jan 2015
-modified 28 Jun 2015
+modified 7 Sept 2017
 by Tom Igoe
 */
 
-var net = require('net'),  // make an instance of the net library
+var net = require('net'), // make an instance of the net library
 clients = new Array,      // array to track clients when they connect
 input = '';               // input string from the keyboard (STDIN)
 
 var stdin = process.openStdin();    // enable input from the keyboard
 stdin.setEncoding('utf8');          // encode everything typed as a string
 
+// this function runs if there's input from the keyboard.
+// you need to hit enter to generate this event.
+stdin.on('data', readKeyboardInput);
+
 var server = net.createServer(listenForClients);  // create the server
+server.listen(8080);                              // start the server listening
 
 // This function is called every time a new client connects:
 function listenForClients(client) {
   console.log('client connected at ' + new Date());
   console.log('client: ' + client.remoteAddress);
-  client.setEncoding('utf8');  // encode everything sent by the client as a string
-  client.write('hello');      // send the client a hello message
-  clients.push(client);       // append the client to the array of clients
+  client.setEncoding('utf8');   // encode everything sent by the client as a string
+  client.write('hello');        // send the client a hello message
+  clients.push(client);         // append the client to the array of clients
 
-  // this function runs if the client sends an 'end' event:
-  client.on('end', function() {
+  function removeClient() {
     console.log('client disconnected');
     var position = clients.indexOf(client); // get the client's position in the array
     clients.splice(position, 1);            // delete it from the array
-  });
-
-  // this function runs if the client sends data:
-  client.on('data', function(data) {
-    data = data.trim();            // trim any whitespace from the string
-    console.log(data + ' ' + new Date());
-    if (data === 'x') {            // if the client sends 'exit',
-    console.log('closing client');
-    client.end();                  // disconnect the client
   }
 
-  // handle any network errors:
-  client.on('error', function (err) {
-    console.error('Network connection error', err);
+  function clientError(error) {
+    console.error('Network connection error', error);
     client.end();
-  });
-
-  function checkClientList() {
-    console.log('cleaning up client list');
-      for (c in clients) {
-        if (typeof(client[c]) === 'undefined') {
-          var position = clients.indexOf(client); // get the client's position in the array
-          clients.splice(position, 1);            // delete it from the array
-        }
-      }
   }
 
-  setInterval(checkClientList, 5000);
-});
+  function readIncoming(data) {
+    data = data.trim();            // trim any whitespace from the string
+    if (data === 'x') {            // if the client sends 'exit',
+      console.log('closing client');
+      client.end();                  // disconnect the client
+    }
+    console.log(client.remoteAddress + ': ' + data); // print what the client said
+  }
 
-// this function runs if there's input from the keyboard.
-// you need to hit enter to generate this event.
-stdin.on('data', function(data) {
+  // client event handlers:
+  client.on('end', removeClient);   // runs if the remote side disconnects
+  client.on('error', clientError);  // runs if there's a socket error
+  client.on('data', readIncoming);  // runs if there's incoming socket data
+
+  // set a function to check the client list periodically:
+  setInterval(checkClientList, 5000);
+}
+
+function checkClientList(clientToDelete) {
+  for (c in clients) {
+    if ((typeof(clients[c]) === 'undefined') ||
+    (clients[c] === clientToDelete)) {
+      console.log('deleting client ' + clients[c].remoteAddress);
+      var position = clients.indexOf(c); // get the client's position in the array
+      clients.splice(position, 1);            // delete it from the array
+    }
+  }
+}
+
+// read incoming keyboard data:
+function readKeyboardInput(data) {
   data = data.trim();                 // trim any whitespace from the string
-  if (data === 'c') {
+  switch (data) {
+    case 'c':
     for (c in clients) {
       console.log(clients[c].remoteAddress);
     }             // list the client array
-  } else {                            // otherwise, broadcast the message to all clients
+    break;
+    default:
+    // otherwise, broadcast the message to all clients
     for (thisClient in clients) {     // iterate over the client array
       clients[thisClient].write(data);// send the message to each client
     }
+    break;
   }
-});
 }
-
-// start the server listening:
-server.listen(8080);
