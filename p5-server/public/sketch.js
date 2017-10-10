8cc6acd0879ee77cc8c47a59cca58eb326b4b935
tigoe <tom.igoe@gmail.com>
2017-10-10 08:03:55 -0400

    tabs to spaces

--- p5-server/public/sketch.js
+++ p5-server/public/sketch.js
@@ -7,19 +7,19 @@
   by Tom Igoe
 */
 
-var responseDiv, sendButton;		// make a button and a text div
-var nameField, ageField;				// text input fields
+var responseDiv, sendButton;    // make a button and a text div
+var nameField, ageField;        // text input fields
 
 function setup() {
-  nameField = createInput('enter your name here');		// create the input fields
+  nameField = createInput('enter your name here');    // create the input fields
   ageField = createInput('enter your age here');
-  sendButton = createButton('Send to server');				// create button
-  responseDiv = createDiv('Waiting for response');		// create text div
-  nameField.position(10, 10);													// position them
+  sendButton = createButton('Send to server');        // create button
+  responseDiv = createDiv('Waiting for response');    // create text div
+  nameField.position(10, 10);                         // position them
   ageField.position(10, 40);
   sendButton.position(10, 70);
   responseDiv.position(10, 100);
-  sendButton.touchEnded(sendRequest);					// give the button a behavior
+  sendButton.touchEnded(sendRequest);          // give the button a behavior
 }
 
 function sendRequest() {
@@ -28,8 +28,8 @@ function sendRequest() {
 }
 
 function getResponse(data) {
-  responseDiv.html(data);							// show the server's response in the div
-  var result = JSON.parse(data);			// parse the response into a JSON object
-  console.log("name:" + result.name);				// get the elements of the object
+  responseDiv.html(data);              // show the server's response in the div
+  var result = JSON.parse(data);       // parse the response into a JSON object
+  console.log("name:" + result.name);        // get the elements of the object
   console.log("comment:" + result.comment);
 }
