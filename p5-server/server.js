570541bae51022f4b4eea19956854b08700d0713
tigoe <tom.igoe@gmail.com>
2017-10-10 07:58:42 -0400

    added minimal server for p5.js project

---
+++ p5-server/server.js
@@ -0,0 +1,28 @@
+/*
+Making Things Talk 3rd ed.
+context: node.js
+*/
+
+var express = require('express');	        // include the express library
+var server = express();					          // create a server using express
+server.use('/',express.static('public')); // serve static files from /public
+
+// respond to GET requests:
+function respondToClient(request, response) {
+  // make a response JSON object to send:
+  var responseBody = {
+    name: request.params.name
+  }
+  // check the age of the client:
+  if (request.params.age > 21) {
+    // add a comment to the response body:
+    responseBody.comment = 'You are old enough to have a drink.';
+  } else {
+    responseBody.comment = 'stay away from the bar';
+  }
+  // send the response:
+  response.end(JSON.stringify(responseBody));
+}
+
+server.listen(8080);                                  // start the server
+server.get('/name/:name/age/:age', respondToClient);  // listen for GET requests
