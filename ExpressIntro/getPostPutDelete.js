503d3a8e7c2bd4dd6475d6f5f6afad543d1774b8
tigoe <tom.igoe@gmail.com>
2018-02-04 09:58:44 -0500

    update to GetPostPutDelete.js

--- ExpressIntro/getPostPutDelete.js
+++ ExpressIntro/getPostPutDelete.js
@@ -1,23 +1,32 @@
 /*
-	Express.js GET/POST example
-	Shows how handle GET, POST, PUT, DELETE
-	in Express.js 4.0
+	Express.js example
+	Shows how handle GET, POST, PUT, DELETE in Express.js 4.0
+
+  Try this with curl as follows:
+  $ curl -X GET http://localhost:8080
+  $ curl -X POST http://localhost:8080
+  $ curl -X PUT http://localhost:8080
+  $ curl -X DELETE http://localhost:8080
+
+  Add any body content you want to the POST or PUT requests using urlencoded
+  strings like so:
+  $ curl -X POST http://localhost:8080 -d 'parameter=value&param2=value2'
 
 	created 14 Feb 2016
-  modified 30 Oct 2017
+  modified 4 Feb 2018
 	by Tom Igoe
 */
 
 var express = require('express');			// include express.js
-var app = express();						      // a local instance of it
+var server = express();						      // a local instance of it
 var bodyParser = require('body-parser');	// include body-parser
 
 // you need a  body parser:
-app.use(bodyParser.urlencoded({extended: false})); // for application/x-www-form-urlencoded
+server.use(bodyParser.urlencoded({extended: false})); // for application/x-www-form-urlencoded
 
 // this runs after the server successfully starts:
 function serverStart() {
-  var port = server.address().port;
+  var port = this.address().port;
   console.log('Server listening on port '+ port);
 }
 
@@ -39,5 +48,6 @@ function handleRequests(request, response) {
 }
 
 // start the server:
-var server = app.listen(8080, serverStart);
-app.all('/*', handleRequests);    // request listener for all types of requests
+server.listen(8080, serverStart);
+// request listener for all types of requests:
+server.all('/*', handleRequests);
