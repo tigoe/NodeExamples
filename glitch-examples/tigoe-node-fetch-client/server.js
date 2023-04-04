f19b5a0a1eab76e302cefa7f7813d97efb891ae5
Tom Igoe <tom.igoe@gmail.com>
2023-04-04 09:44:10 -0400

    update node fetch client

--- glitch-examples/tigoe-node-fetch-client/server.js
+++ glitch-examples/tigoe-node-fetch-client/server.js
@@ -2,8 +2,14 @@
  tigoe-node-fetch-client
  
  This example shows how to use node.js to make requests to a RESTful API and simplify the result and serve it as a RESTful API. It assumes you're using a microcontroller-based client or have some other reason for needing the results of an API to be simplified. 
+ 
+ The full query will look like this: https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=latest&station=8518750&product=water_level&datum=MTL&time_zone=lst_ldt&units=metric&format=json
+ 
+ You can use the NOAA URL generator to design other queries:
+ https://tidesandcurrents.noaa.gov/api-helper/url-generator.html
   
   created 9 Jan 2023
+  modified 4 Apr 2023
   by Tom Igoe
 */
 
@@ -15,12 +21,12 @@ const fetch = require("node-fetch");
 // value for the API request:
 let requestUrl = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter";
 // and the query string:
-let query = {
-  date: "latest",
-  station: "8518750",
-  product: "water_level",
-  datum: "MTL",
-  time_zone: "lst",
+let myQuery = {
+  date: "latest", // get the latest data
+  station: "8518750", // the Battery, NYC
+  product: "water_level", // we want water levl
+  datum: "MTL", //  mean tide level
+  time_zone: "lst_ldt", // local time, standard or daylight savings
   units: "metric",
   format: "json",
 };
@@ -37,15 +43,16 @@ const params = {
 function handleGet(request, response) {
   // iterate over the request query:
   for (let p in request.query) {
-    // if there's a matching property in query,
-    if (query.hasOwnProperty(p)) {
+    // if there's a matching property in the query,
+    if (myQuery.hasOwnProperty(p)) {
       // then replace it with this new value:
-      query[p] = request.query[p];
+      myQuery[p] = request.query[p];
     }
   }
   // convert the query to a string and attach to URL:
   let thisUrl = requestUrl;
-  let queryString = new URLSearchParams(Object.entries(query)).toString();
+  // convert myQuery from JSON to a search param string:
+  let queryString = new URLSearchParams(myQuery).toString();
   thisUrl += "?" + queryString;
 
   // make a request to the remote API using fetch:
@@ -72,7 +79,7 @@ function handleGet(request, response) {
     // if there's a successful result from the remote API:
     let myData = result.data[0];
     let dataToReturn = {};
-    // get just the time  and the water level:
+    // get just the time and the water level:
     dataToReturn.time = myData.t;
     dataToReturn.waterLevel = myData.v;
     // send this in response to the original request:
