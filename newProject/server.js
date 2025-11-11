96f20699bba3ea4da44050d16064feaa543a480f
Tom Igoe <tom.igoe@gmail.com>
2025-11-11 10:38:03 -0500

    update serve.rjs

--- newProject/server.js
+++ newProject/server.js
@@ -1,7 +1,16 @@
 var express = require('express');	// include the express library
 var server = express();			// create a server using express
-server.use('/',express.static('public')); // serve static files from /public
-console.log("hello I am running");
+//server.use('/',express.static('public')); // serve static files from /public
 
-server.listen(process.env.PORT || 8080);  // start the server
+function serverStart() {
+  console.log("hello I am running");
+}
 
+function handleGarden(request, response) {
+  console.log(request);
+  response.send("Here's a garden!");
+  response.end();
+}
+
+server.listen(8080, serverStart);  // start the server
+server.get("/garden", handleGarden);
