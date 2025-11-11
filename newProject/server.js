8b94f9071fe3c325c891fc8e5dc5fb6b269016a4
Tom Igoe <tom.igoe@gmail.com>
2025-11-11 11:00:51 -0500

    Update server.js

--- newProject/server.js
+++ newProject/server.js
@@ -12,5 +12,48 @@ function handleGarden(request, response) {
   response.end();
 }
 
+function handleFlowers(request, response) {
+  var flowerJson = {
+    "petals": 8,
+    "stem": 3,
+    "seeds": 1000000
+  };
+  console.log(request);
+  response.json(flowerJson);
+  response.end();
+}
+
+function handleWorms(request, response) {
+  var wormJson = {
+    "wormCount": 1000,
+  }
+  console.log(request);
+  response.json(wormJson);
+  response.end();
+}
+
+function handleBees(request, response) {
+  var beeJson = {
+    "beeCount": 100000000,
+    "beeHave": "Oh!"
+  }
+  console.log(request);
+  response.json(beeJson);
+  response.end();
+}
+
+function handleFertilizer(request, response) {
+  var fertilizerJson = {
+    "poop": 1
+  }
+  console.log(request);
+  response.json(fertilizerJson);
+  response.end();
+}
+
 server.listen(8080, serverStart);  // start the server
 server.get("/garden", handleGarden);
+server.get("/garden/flowers", handleFlowers);
+server.get("/garden/worms", handleWorms);
+server.get("/garden/bees", handleBees);
+server.post("/garden/fertilizer", handleFertilizer);
